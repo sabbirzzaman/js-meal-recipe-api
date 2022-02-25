11f05a3df9470bc2436c2944aeffc735970888f4
sabbirzzaman <sztonmoy7@gamil.com>
2022-02-25 16:56:09 +0600

    fetch api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,13 @@
-console.log('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+console.log('haha');
 
 const getMeal = () => {
-    
+  fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
+    .then((res) => res.json())
+    .then((data) => displayMeal(data));
+};
+
+const displayMeal = (foods) => {
+    foods.meals.forEach(meal => {
+        console.log(meal)
+    })
 }
